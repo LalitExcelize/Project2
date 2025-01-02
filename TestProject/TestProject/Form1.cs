8ec4c58a8dfb7aff4750ef77bee268c7ed1d9344
LalitExcelize <lalit@excelize.com>
2025-01-02 17:49:00 +0530

    New commit
    comment edited

--- TestProject/TestProject/Form1.cs
+++ TestProject/TestProject/Form1.cs
@@ -17,7 +17,7 @@ namespace TestProject
             // Branch 2
             MessageBox.Show("Message 2"); // New Message in Branch 2
             MessageBox.Show("Message 3"); // New Message in Branch 2
-            MessageBox.Show("Message 4"); // New Message in Branch 3
+            MessageBox.Show("Message 4"); // New Message in Branch 2
         }
     }
 }
